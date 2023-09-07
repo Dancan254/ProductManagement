0576265268a03dadabc00da9f9cbd1366a1bff3b
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-07 19:02:14 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        ProductService service = new ProductService();
+        service.addProduct(new Product("MacBook Air", "MacOs", "Kenya", 2023));
+        service.addProduct(new Product("MacBook Air", "MacOS", "Kenya", 2023));
+        service.addProduct(new Product("iPhone 13", "iOS", "USA", 2023));
+        service.addProduct(new Product("Samsung Galaxy S22", "Android", "South Korea", 2023));
+        service.addProduct(new Product("Dell XPS 13", "Windows", "USA", 2023));
+        service.addProduct(new Product("Sony PlayStation 5", "Custom OS", "Japan", 2023));
+        service.addProduct(new Product("HP Spectre x360", "Windows", "USA", 2023));
+        service.addProduct(new Product("Google Pixel 6", "Android", "USA", 2023));
+        service.addProduct(new Product("Lenovo ThinkPad X1 Carbon", "Windows", "China", 2023));
+        service.addProduct(new Product("Amazon Echo Show 10", "Amazon Alexa", "USA", 2023));
+        service.addProduct(new Product("Nintendo Switch Pro", "Custom OS", "Japan", 2023));
+
+//        Set<Product> list = service.getProducts();
+//        for (Product pr : list){
+//            System.out.println(pr + " ");
+//        }
+        //iterate through the list
+//        var itr = list.iterator();
+//        while (itr.hasNext()){
+//            System.out.println(itr.next() + " ");
+//        }
+
+        System.out.println(service.getSpecificProduct("lenovo ThinkPad X1 Carbon"));
+    }
 }
--- src/ProductService.java
+++ src/ProductService.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ProductService {
-    
+
+    Set<Product> products = new HashSet<>();
+
+    //METHOD TO ADD PRODUCT
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    //METHOD TO GET ALL PRODUCTS
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    //method to access a specific product
+    public Product getSpecificProduct(String name){
+
+        //iterate through the products
+        for (Product p : products)
+            if (p.getName().equalsIgnoreCase(name)){
+            return p;
+        }
+        return null;
+    }
+
 }
